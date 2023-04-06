a321cd667efb4fdef3139bc030330f2923534e06
quyeths <diesmiling2512@gmail.com>
2023-04-06 20:58:58 +0700

    fix tabnine

---
+++ lua/plugins/cmp-tabnine.lua
@@ -0,0 +1,32 @@
+return {
+  {
+    "tzachar/cmp-tabnine",
+    lazy = false,
+    build = "./install.sh",
+    dependencies = "hrsh7th/nvim-cmp",
+    config = function()
+      require("cmp").setup({
+        sources = {
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "buffer" },
+          { name = "path" },
+          { name = "cmp_tabnine" },
+        },
+      })
+      require("cmp_tabnine.config"):setup({
+        max_lines = 1000,
+        max_num_results = 20,
+        sort = true,
+        run_on_every_keystroke = true,
+        snippet_placeholder = "..",
+        ignored_file_types = {
+          -- default is not to ignore
+          -- uncomment to ignore in lua:
+          -- lua = true
+        },
+        show_prediction_strength = false,
+      })
+    end,
+  },
+}
