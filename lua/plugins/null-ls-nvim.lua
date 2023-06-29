1b2062e60c5711de4ee0650cf6e847c6f23ce143
quyeths <diesmiling2512@gmail.com>
2023-06-29 15:47:55 +0700

    disable formatting.fish_indent and diagnostics.fish

--- lua/plugins/null-ls-nvim.lua
+++ lua/plugins/null-ls-nvim.lua
@@ -8,8 +8,8 @@ return {
       return {
         root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
         sources = {
-          nls.builtins.formatting.fish_indent,
-          nls.builtins.diagnostics.fish,
+          -- nls.builtins.formatting.fish_indent,
+          -- nls.builtins.diagnostics.fish,
           nls.builtins.formatting.stylua,
           nls.builtins.formatting.prettier,
           nls.builtins.formatting.shfmt,
