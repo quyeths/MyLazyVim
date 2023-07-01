d8c1dab3e9b99004b7ec58323ae5c605f96ce918
quyeths <diesmiling2512@gmail.com>
2023-07-02 00:10:35 +0700

    config media-files

--- lua/plugins/telescope-media-files.lua
+++ lua/plugins/telescope-media-files.lua
@@ -1,19 +1,22 @@
 return {
   {
     "nvim-telescope/telescope-media-files.nvim",
-    config = function()
-      require("telescope").load_extension("media_files")
-      require("telescope").setup({
-        extensions = {
-          media_files = {
-            -- filetypes whitelist
-            -- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
-            filetypes = { "png", "webp", "jpg", "jpeg" },
-            -- find command (defaults to `fd`)
-            find_cmd = "fd",
-          },
-        },
-      })
-    end,
+    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim", "nvim-lua/popup.nvim" },
+  config = function()
+    require("telescope").load_extension("media_files")
+  end,
+    -- opts = {
+    --   require("telescope").setup({
+    --     extensions = {
+    --       media_files = {
+    --         -- filetypes whitelist
+    --         -- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
+    --         filetypes = { "png", "webp", "jpg", "jpeg" },
+    --         -- find command (defaults to `fd`)
+    --         find_cmd = "rg",
+    --       },
+    --     },
+    --   }),
+    -- },
   },
 }
